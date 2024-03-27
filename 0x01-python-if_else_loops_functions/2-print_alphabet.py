4b596ca8b5abcbe4b974bb96bc272c8aa41cfd4d
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-03-27 10:45:07 -0700

    2-print_alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,5 @@
 # Program print ASCII alphabet in lowercase, not followed by a new line.
 
 for lower in range(97, 123):
-    print("{}".format(chr(lower)), end="")
+    if lower < 123:
+        print("{}".format(chr(lower)), end="")
