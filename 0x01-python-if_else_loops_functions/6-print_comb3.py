d8bd5699218c6408c2c676b5f36a744d06d701ed
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-03-28 07:21:02 -0700

    6-print_comb-no

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for comb in range(0, 10):
     for comb2 in range(comb, 10):
         if comb == 8 and comb2 == 9:
             print("{}{}".format(comb, comb2))
-        if comb != comb2:
+        elif comb != comb2:
             print("{}{}".format(comb, comb2), end=", ")
         pass
