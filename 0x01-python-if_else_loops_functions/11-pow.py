9e839018bd63bf2e36c07d17f88d219ebb2642be
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-03-29 07:28:35 -0700

    11-power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b > 0:
-        exponent = 1
-        for _ in range(b):
-            exponent = exponent * a
-    elif b == 0:
-        exponent = 1
-        for _ in range(b):
-            exponent = exponent * (a / a)
-    else:
-        exponent = 1.0
-        for _ in range((b) * -1):
-            exponent = exponent / a
-    return (exponent)
+    return (a ** b)
