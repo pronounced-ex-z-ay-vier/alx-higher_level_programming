b9e40a13ceeb7e5ba3ec4cbd9af86cef20d3a974
pronounced-ex-z-ay-vier <obiajulujosephpeter2@gmail.com>
2024-03-28 17:03:57 -0700

    9-print_last_digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    digits = number % 10
+    if number > 0:
+        digits = number % 10
+    if number < 0:
+        digits = (number * -1) % 10
+    print("{}".format(digits), end="")
+    return(digits)
